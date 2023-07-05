e0271b85932a0393b3b6e0c1ef381eb0053fe679
anaclrra <anaclarab826@gmail.com>
2023-07-05 15:43:10 -0300

    Método Add

--- fila_prioridade.py
+++ fila_prioridade.py
@@ -18,7 +18,6 @@ class FilaPrioridade:
 
     # Retorna True se a fila de prioridade está vazia, False caso contrário
     def is_empty(self) -> bool:
-        # implementação do método
         pass
 
     
@@ -30,21 +29,40 @@ class FilaPrioridade:
     # Retorna uma referência para o primeiro item da fila de prioridade
     # Caso a lista esteja vazia, retorna None
     def first(self) -> No:
-        # implementação do método
         pass
 
 
     # insere um item na fila de prioridade e retorna True, se o item for inserido
     # se a fila de prioridade estiver cheia, lança uma exceção: raise Exception("mensagem de erro")
     def add(self, valor, prioridade) -> bool:
-        # implementação do método
-        pass
+        no = No(valor, prioridade)
 
+        if self.is_full():
+            raise Exception("Lista cheia!")
+
+        elif self.is_empty():
+            self.__inicio = no
+        
+        elif prioridade > self.__inicio.prioridade:
+            no.prox = self.__inicio
+            self.__inicio = no
+
+        else:    
+            pointer = self.__inicio
+            while pointer.prox and prioridade <= pointer.prox.prioridade:
+                pointer = pointer.prox
+
+            no.prox = pointer.prox
+            pointer.prox = no
+
+        self.__qtdItens += 1    
+        return True
+    
+        
     
     # remove o primeiro item da fila de prioridade, caso não esteja vazia, e retorna o Nó
     # se a fila de prioridade estiver vazia, lança uma exceção: raise Exception("mensagem de erro")
     def remove(self) -> No:
-        # implementação do método
         pass
 
 
@@ -53,7 +71,6 @@ class FilaPrioridade:
     # caso a fila de prioridade esteja vazia, imprime uma mensagem informando
     # que a fila de prioridade está vazia e retorna uma lista vazia
     def display(self) -> list[tuple()]:
-        # implementação do método
         pass
     
 
